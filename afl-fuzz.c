1f1637d363fb3dccd7979406e793ba0c304edb99
LeeHun9 <08173076@cumt.edu.cn>
2023-05-11 08:20:24 -0400

    stat_record

--- afl-fuzz.c
+++ afl-fuzz.c
@@ -353,6 +353,11 @@ static inline u8 has_new_bits(u8* virgin_map);
 
 /* AFLNet-specific variables & functions */
 
+// leehung
+static u8* stats_log_path = NULL;
+FILE* fp_stats = NULL;
+s32 pre_time = 0;
+
 u32 server_wait_usecs = 10000;
 u32 poll_wait_msecs = 1;
 u32 socket_timeout_usecs = 1000;
@@ -5252,6 +5257,17 @@ static void show_stats(void) {
 
   } else SAYF("\r");
 
+  /*leehung: Record stats to file each 3 seconds*/
+  const int period = 3;
+  int rec = 0;
+  u64 _delta = cur_ms - start_time;
+  s32 _second = (_delta / 1000) % 60;
+  if (_second % period == 0 && pre_time != _second) {
+    fprintf(fp_stats, "%llu,%-21s,%-5s,%0.02f%%,%s\n", cur_ms - start_time, DI(total_execs), DI(queued_paths), t_byte_ratio,DI(unique_crashes));
+    pre_time = _second;
+    rec = 1;
+  }
+
   /* Show debugging stats for AFLNet only when AFLNET_DEBUG environment variable is set */
   if (getenv("AFLNET_DEBUG") && (atoi(getenv("AFLNET_DEBUG")) == 1) && state_aware_mode) {
     SAYF(cRST "\n\nMax_seed_region_count: %-4s, current_kl_messages_size: %-4s\n\n", DI(max_seed_region_count), DI(kl_messages->size));
@@ -9126,6 +9142,15 @@ int main(int argc, char** argv) {
         usage(argv[0]);
 
     }
+
+  /*leehung: 记录fuzzing状态文件，*/
+  stats_log_path = alloc_printf("%s/stats_record.csv", out_dir);
+  fp_stats = fopen(stats_log_path, "w");
+  if (fp_stats == NULL) {
+    FATAL("Failed to open stats_record.csv");
+  }
+  // write head to csv
+  fprintf(fp_stats, "run time,total execs,total paths,map density,unique crashes\n");
 
   if (optind == argc || !in_dir || !out_dir) usage(argv[0]);
 
